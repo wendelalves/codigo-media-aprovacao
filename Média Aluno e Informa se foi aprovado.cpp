29fa48f090d68d9ab11b47185bd12b9000875be0
Wendelalves <wendelfa2@gmail.com>
2021-02-05 22:45:51 -0300

    adicionado pt-br a liguagem

--- Média Aluno e Informa se foi aprovado.cpp
+++ Média Aluno e Informa se foi aprovado.cpp
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <locale.h>
 int main(void) {
   float nota1;
   float nota2;
@@ -7,8 +8,8 @@ int main(void) {
   float media;
   int i;
   char nome;
-    printf("Esse codigo faz o calculo da media e informa se o aluno foi aprovado (media >= 6.0), reprovado (media <= 5.0) ou recuperacao (media >= 5.0) && (media <= 6.0).");
-
+  setlocale(LC_ALL, "Portuguese");
+  printf("Esse codigo faz o cálculo da média e informa se o aluno foi aprovado (média >= 6,0), reprovado (média <= 5,0) ou recuperação (média >= 5,0) && (média <= 6,0). Separe os números reais por vírgula.");
   printf("\n\nEntre com o nome do aluno: ");
   scanf("%s", &nome);
   printf("Informe a 1a. nota:\n");
@@ -20,11 +21,11 @@ int main(void) {
   printf("\nInforme a 3a. nota:\n");
   scanf("%f", &nota4);
   media = (nota1 + nota2 + nota3 + nota4) / 4;
-  printf("\nMedia final = %.2f", media);
+  printf("\nMédia final = %.2f", media);
   if (media >= 6.0)
      printf(" (aluno aprovado).");
   else if ((media >= 5.0) && (media < 6.0))
-          printf(" (aluno em recuperacao).");
+          printf(" (aluno em recuperação).");
        else
-      printf(" (aluno reprovado).");
+      printf(". (aluno reprovado).");
 }
